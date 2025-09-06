c6421abde15e058cb0b49489661ef67d0e8d5221
surojitghosh <contact@surojit.in>
2025-09-07 01:25:48 +0530

    fix(ui): minor ux fixes

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import Container from "./container";
 import Link from "next/link";
-import { buttonVariants } from "./ui/button";
+import { Button, buttonVariants } from "./ui/button";
 import { USER } from "@/lib/config";
 import { cn } from "@/lib/utils";
 import { Icons } from "./icons";
@@ -37,7 +37,7 @@ const Header = () => {
                     </h1>
                 </Link>
 
-                <div className="flex items-center gap-2">
+                <div className="flex items-center gap-4">
                     {/* Desktop Navigation */}
                     <div className="hidden space-x-4 md:flex">
                         {links.map((link: Link) => (
@@ -51,48 +51,44 @@ const Header = () => {
                         ))}
                     </div>
 
-                    <Link
-                        href={"https://github.com/surojit-ghosh/surojit.in"}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className={cn(
-                            buttonVariants({ variant: "outline", size: "icon" }),
-                            "size-8 cursor-pointer rounded-full"
-                        )}
-                    >
-                        <Icons.github className="size-4" />
-                    </Link>
-                    <ThemeToggle />
+                    <div className="flex items-center gap-2">
+                        <Link
+                            href={"https://github.com/surojit-ghosh/surojit.in"}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className={cn(
+                                buttonVariants({ variant: "outline", size: "icon" }),
+                                "size-8 cursor-pointer rounded-full"
+                            )}
+                        >
+                            <Icons.github className="size-4" />
+                        </Link>
+                        <ThemeToggle />
 
-                    {/* Mobile Navigation */}
-                    <div className="md:hidden">
-                        <DropdownMenu>
-                            <DropdownMenuTrigger asChild>
-                                <button
-                                    className={cn(
-                                        buttonVariants({ variant: "outline", size: "icon" }),
-                                        "size-8 cursor-pointer rounded-full"
-                                    )}
-                                >
-                                    <Menu className="size-4" />
-                                    <span className="sr-only">Toggle menu</span>
-                                </button>
-                            </DropdownMenuTrigger>
-                            <DropdownMenuContent
-                                side="bottom"
-                                align="end"
-                                sideOffset={8}
-                                className=""
-                            >
-                                {links.map((link) => (
-                                    <DropdownMenuItem key={link.href} asChild>
-                                        <Link href={link.href} className="w-full">
-                                            {link.name}
-                                        </Link>
-                                    </DropdownMenuItem>
-                                ))}
-                            </DropdownMenuContent>
-                        </DropdownMenu>
+                        {/* Mobile Navigation */}
+                        <div className="md:hidden">
+                            <DropdownMenu>
+                                <DropdownMenuTrigger asChild>
+                                    <Button
+                                        size={"icon"}
+                                        variant="outline"
+                                        className="size-8 cursor-pointer rounded-full"
+                                    >
+                                        <Menu className="size-4" />
+                                        <span className="sr-only">Toggle menu</span>
+                                    </Button>
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent side="bottom" align="end" sideOffset={8}>
+                                    {links.map((link) => (
+                                        <DropdownMenuItem key={link.href} asChild>
+                                            <Link href={link.href} className="w-full">
+                                                {link.name}
+                                            </Link>
+                                        </DropdownMenuItem>
+                                    ))}
+                                </DropdownMenuContent>
+                            </DropdownMenu>
+                        </div>
                     </div>
                 </div>
             </Container>
--- components/sections/hero/index.tsx
+++ components/sections/hero/index.tsx
@@ -1,17 +1,8 @@
 import { USER } from "@/lib/config";
-import Image from "next/image";
 
 const HeroSection = () => {
     return (
         <div className="flex items-center gap-4">
-            <Image
-                className="rounded-full"
-                src="/surojit.png"
-                alt="Hero Image"
-                width={100}
-                height={100}
-            />
-
             <div className="space-y-1">
                 <h1 className="text-lg font-medium sm:text-xl">Hey, I&apos;m {USER.name}</h1>
                 <h2 className="text-muted-foreground text-sm">{USER.description}</h2>
