3ba6abf690449bad1e3468268da5ed6399013b75
surojitghosh <contact@surojit.in>
2025-09-17 22:52:51 +0530

    add: url shortner photo

--- components/divider.tsx
+++ components/divider.tsx
@@ -6,7 +6,7 @@ const Divider: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({
     className?: string;
 }) => {
     return (
-        <div className={cn(className, "border-b")}>
+        <div className={cn(className, "border-b px-2")}>
             <div className={"mx-auto w-full max-w-3xl border-x"}>
                 <div className="h-6 bg-[image:repeating-linear-gradient(315deg,_var(--muted)_0,_var(--muted)_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed" />
             </div>
