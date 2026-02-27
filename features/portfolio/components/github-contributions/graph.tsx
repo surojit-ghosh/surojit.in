1c44876562f145c4740a94e11bb26969229761f6
surojitghosh <contact@surojit.in>
2026-02-28 00:26:05 +0530

    style: github contributions style

--- features/portfolio/components/github-contributions/graph.tsx
+++ features/portfolio/components/github-contributions/graph.tsx
@@ -29,6 +29,13 @@ export function GitHubContributionGraph({ contributions }: { contributions: Acti
                                     activity={activity}
                                     dayIndex={dayIndex}
                                     weekIndex={weekIndex}
+                                    // className={cn(
+                                    //     "data-[level='0']:fill-[#ebedf0] dark:data-[level='0']:fill-[#161b22]",
+                                    //     "data-[level='1']:fill-[#9be9a8] dark:data-[level='1']:fill-[#0e4429]",
+                                    //     "data-[level='2']:fill-[#40c463] dark:data-[level='2']:fill-[#006d32]",
+                                    //     "data-[level='3']:fill-[#30a14e] dark:data-[level='3']:fill-[#26a641]",
+                                    //     "data-[level='4']:fill-[#216e39] dark:data-[level='4']:fill-[#39d353]"
+                                    // )}
                                 />
                             </g>
                         </TooltipTrigger>
